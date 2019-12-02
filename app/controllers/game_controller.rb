43b9d81cf637bd4e0e175e131a5c92191954653d
Kathleen Kamali <kathleenkamali@gmail.com>
2019-12-02 11:57:08 -0800

    more game routes

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -15,4 +15,23 @@ class GameController < ApplicationController
 
     redirect '/games'
   end
+
+  get '/games/:id/edit' do
+    @game = Game.find(params[:id])
+
+    erb :'games/edit'
+  end
+
+  post '/games/:id/edit' do
+    game = Game.find(params[:id])
+    game.update(params)
+
+    redirect '/games/:id'
+  end
+
+  get '/games/:id' do
+    @game = Game.find(params[:id])
+
+    erb :'games/show'
+  end
 end
